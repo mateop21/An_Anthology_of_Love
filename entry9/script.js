c70a4b850dee15bc4a1a26d995b21348ce7ecb65
mateop21 <114003887+mateop21@users.noreply.github.com>
2022-10-26 09:54:34 -0400

    update10/26

---
+++ entry9/script.js
@@ -0,0 +1,10 @@
+var words = ['Cook a meal for someone, you care about', 'make hot chocolate for someone you care about',
+    'go to the park, bring a friend','write a letter for someone',
+    'hug someone you love','call someone you love and tell them how much you appreciate them']
+var index = Math.random() * 5
+index = Math.floor(index)
+console.log( words[index])
+
+var place = document.getElementById('place')
+console.log(place)
+place = innertext(words[index])
